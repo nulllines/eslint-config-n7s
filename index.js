8f05e503bcd9e3dc898e6e26c5b57c9cd4b332ca
Max Holman <max@holmn.com>
2016-08-19 17:57:31 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,125 @@
+module.exports = {
+    env: {
+        browser: false,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    rules: {
+        indent: [
+            'error',
+            4,
+            {
+                'SwitchCase': 1
+            }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'always',
+        ],
+        'no-bitwise': 'error',
+        'curly': [
+            'error',
+            'all'
+        ],
+        'eqeqeq': 'error',
+        'guard-for-in': 'error',
+        'wrap-iife': 'error',
+        'no-caller': 'error',
+        'no-new': 'error',
+        'strict': [
+            'error',
+            'safe'
+        ],
+        'no-use-before-define': [
+            'error',
+            {
+                'functions': false
+            }
+        ],
+        'space-before-function-paren': [
+            'error',
+            {
+                'anonymous': 'always',
+                'named': 'never'
+            }
+        ],
+        'func-call-spacing': ['error', 'never'],
+        'array-bracket-spacing': [
+            'error',
+            'never',
+            {}
+        ],
+        'space-in-parens': [
+            'error',
+            'never'
+        ],
+        'quote-props': [
+            'error',
+            'as-needed'
+        ],
+        'key-spacing': [
+            'error',
+            {
+                'beforeColon': false,
+                'afterColon': true
+            }
+        ],
+        'space-unary-ops': [
+            'error',
+            {
+                'words': false,
+                'nonwords': false
+            }
+        ],
+        'no-mixed-spaces-and-tabs': 'error',
+        'no-trailing-spaces': 'error',
+        'yoda': [
+            'error',
+            'never'
+        ],
+        'no-with': 'error',
+        'brace-style': [
+            'error',
+            '1tbs',
+            {
+                'allowSingleLine': true
+            }
+        ],
+        'no-multiple-empty-lines': 'error',
+        'no-multi-str': 'error',
+        'one-var': [
+            'error',
+            'never'
+        ],
+        'space-before-blocks': [
+            'error',
+            'always'
+        ],
+        'comma-style': [
+            'error',
+            'last'
+        ],
+        'space-infix-ops': 'error',
+        'eol-last': 'error',
+        'dot-notation': 'error',
+        'keyword-spacing': [
+            'error'
+        ],
+        'lines-around-comment': [
+            'error',
+            {
+                'beforeLineComment': true
+            }
+        ]
+    },
+};
